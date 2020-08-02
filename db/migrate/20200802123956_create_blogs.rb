65dd85a73c090f8e62da258413ee73a6f1dcc85f
Nirankar Singh <nirankarsingh247@gmail.com>
2020-08-02 18:30:04 +0530

    add blog/devise and haml gem

--- db/migrate/20200802123956_create_blogs.rb
+++ db/migrate/20200802123956_create_blogs.rb
@@ -1,6 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
+      t.string :title
+      t.text :article
+      t.boolean :status
+
 
       t.timestamps
     end
